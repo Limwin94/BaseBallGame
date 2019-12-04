0cbaf4c2cae980e8c23eb9a4a182e6be369ab476
limwin94 <limwin94@gmail.com>
2019-12-04 21:40:19 +0900

    2단계 요구사항 2: 시합하기 기능 구현
    경기진행 함수 수정, 타자의 행동패턴 확률 함수 추가, 점수 합산 기능 추가.
    공수교대후 시합시 오류(스택오버플로우로 추정) 수정 예정.

--- baseball_01.c
+++ baseball_01.c
@@ -20,12 +20,12 @@ typedef struct{
 int init_menu();
 void insert_team(TEAM *, int);
 void print_team(TEAM *, int);
-/********************/
 
+/* 경기 진행                */
 void init_playing(TEAM *, int);
-int playing(TEAM *, int tnum);
+char playing(TEAM *, int , int);
 void clear(int *, int *);
-void next_alarm(int);
+void score(int *, int, int);
 
 void main(){
     int menu_result;
@@ -41,6 +41,7 @@ void main(){
         switch(menu_result)
         {
             case 1:
+                memset(team, 0x00, sizeof(team));
                 insert_team(team, 2);
                 break;
             case 2:
@@ -75,9 +76,9 @@ int init_menu(){
     return sel;
 }
 
+/* 팀정보 삽입 */
 void insert_team(TEAM * team, int len)
 {
-    memset(team, 0x00, sizeof(TEAM));
     int data_size = sizeof(team[0].data)/sizeof(TEAM_DATA);
     char t_name[128];
     char name[64];
@@ -119,6 +120,7 @@ void insert_team(TEAM * team, int len)
     printf("\n팀 데이터 입력이 완료되었습니다.\n");
 }
 
+/* 팀 정보 출력*/
 void print_team(TEAM * team, int len)
 {
     int data_size = sizeof(team[0].data)/sizeof(TEAM_DATA);
@@ -136,110 +138,146 @@ void print_team(TEAM * team, int len)
 /* 경기진행                              */
 /****************************************/ 
 void init_playing(TEAM * team, int len){
-    int result;
+    char result;
     
     int strike = 0, ball = 0, out = 0, hit = 0;
-    int c1 = 1, c2 = 0, flag = 0, s1, s2;
+    int c1 = 1, c2 = 0; //경기회수, 팀번호(0,1), 
+    int ply = 0;        //선수번호
+    int scr[2] = {0, }; //점수
 
     printf("%.*s VS %.*s의 시합을 시작합니다.\n", strlen(team[0].teamName), team[0].teamName, strlen(team[1].teamName), team[1].teamName);
     
     while(1)
     {
-        //6회 종료후 7회일때 동점일 경우 무승부처리
-        if(c1 == 7 && s1 == s2)
+        //6회 종료후 게임 종료.
+        if(c1 == 7)
         {
             printf("경기 종료\n");
             break;
         }
-
-        //c2 == 0 : 초, c2 == 1: 말
-        if(c2 == 0){
-            printf("%d초 %.*s 공격", c1, strlen(team[c2].teamName), team[c2].teamName);
-        } else {
-            printf("%d말 %.*s 공격", c1, strlen(team[c2].teamName), team[c2].teamName);
-            c1++; //X회
+        //c2 == 0 : 초(1팀), c2 == 1: 말(2팀)
+        if(out == 3){
+            c2 = ~c2;
+            if(c2 == 0){
+                printf("%d초 %.*s 공격", c1, strlen(team[c2].teamName), team[c2].teamName);
+            } else {
+                printf("%d말 %.*s 공격", c1, strlen(team[c2].teamName), team[c2].teamName);
+                c1++; //X회
+            }
+            clear(&strike, &ball); clear(&hit, &ply);
+            out = 0;
         }
 
-        result = playing(team, c2);
+        result = playing(team, c2, ply);
+        printf("%d번 %.*s\n", team[c2].data[ply].num, strlen(team[c2].data[ply].name), team[c2].data[ply].name);
 
         switch(result)
         {
-            case 0:
+            case 's':
                 strike++;
                 printf("스트라이크!\n");
                 if(strike == 3)
                 {
                     out++;
+                    ply++;
                     clear(&strike, &ball);
                 }
                 break;
-            case 1:
+            case 'b':
                 ball++;
                 printf("볼!\n");
                 if(ball == 4)
                 {
                     hit++;
+                    ply++;
                     clear(&strike, &ball);
                 }
                 break;
-            case 2:
+            case 'o':
+                printf("아웃!\n");
                 out++;
+                ply++;
                 clear(&strike, &ball);
-                if(out == 3){
-                    printf("아웃!\n");
-                    flag = 1;
-                }
                 break;
-            case 3:
+            case 'h':
+                printf("안타!\n");
                 hit++;
                 clear(&strike, &ball);
                 break;
         }
-        
-        if(out != 3)
-            next_alarm(result);
-        
-        printf("%dS %dB %dO\n\n", strike, ball, out);
-    
-        if(flag == 1)
-        {
-            printf("최종 안타수 : %d\fnGAME OVER\n", hit);
-            break;
+
+        printf("%dS %dB %dO %dH\n\n", strike, ball, out, hit);
+        if(ply > 3)
+            ply = 0;
+
+        score(scr, c2, hit);
+    }
+
+     printf("%.*s VS %.*s\n", strlen(team[0].teamName), team[0].teamName, strlen(team[1].teamName), team[1].teamName);
+     printf("%d : %d\n Thank you!\n", scr[0], scr[1]);
+     exit(0);
+}
+
+/*************************************************/
+/* 0: 스트라이크, 1:볼, 2: 아웃, 3: 안타 확률계산
+-안타: h, 0.1 < h < 0.5
+-스트라이크: (1 - h) / 2 - 0.05
+-볼: (1 - h) / 2 - 0.05
+-아웃: 0.1
+/************************************************/ 
+char playing(TEAM * team, int tnum, int pnum)
+{
+    double hit = team[tnum].data[pnum].hit_rate;
+    double stk = (1-hit)/2 - 0.05;
+    double ball = stk;
+    double out = 0.1;
+    double cal = 0.0, dtemp = 0.0;
+    char ctemp;
+    //맵핑을 위해 만들었으나 개선필요.
+    double value[4] = {out, hit, stk, ball};
+    char key[4] = {'o', 'h', 's', 'b'};
+    int i, j;
+
+    double r = rand() / (double)32767; //0~1까지 실수형 출력
+    //오름차순정렬
+    for (i = 0; i < 4; i++)
+    {
+		for (j = 0; j < 3; j++)
+		{
+        	if (value[j] > value[j + 1]) 
+            {
+				dtemp = value[j];
+                ctemp = key[j];
+				value[j] = value[j + 1];
+                key[j] = key[j + 1];
+				value[j + 1] = dtemp;
+                key[j + 1] = ctemp;
+			}
         }
-        
-        c2 = ~c2;
     }
-}
 
-/****************************************/
-/* 0: 스트라이크, 1:볼, 2: 아웃, 3: 안타  */
-/****************************************/ 
-int playing(TEAM * team, int tnum)
-{
-    
-}
-
-/****************************************/
-/* 스트라이크, 볼 갯수 초기화             */
-/****************************************/ 
-void clear(int * strike, int * ball)
-{
-    *strike = 0;
-    *ball = 0;
-}
-
-/****************************************/
-/* 타자 입장 알림                        */
-/****************************************/ 
-void next_alarm(int state)
-{
-    if(state == 2)
+    for(i=0; i<4; i++)
     {
-        printf("아웃! 다음 타자가 타석에 입장했습니다.\n");
+        cal += value[i];
+        if(r <= cal)
+            return key[3-i];
     }
-    else if(state == 3)
-    {
-        printf("안타! 다음 타자가 타석에 입장했습니다.\n");
-    }
+
+
+}
+
+/****************************************/
+/* 0으로 초기화             */
+/****************************************/ 
+void clear(int * num1, int * num2)
+{
+    *num1 = 0;
+    *num2 = 0;
+}
+
+void score(int * scr, int tnum, int hit)
+{
+    if(hit >= 4)
+        scr[tnum]++;
 }
 
